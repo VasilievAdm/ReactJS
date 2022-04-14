784d9149ddb1739f6ef2606bcb04725df2b82e6f
VasilievAdm <vasiliev.adm@gmail.com>
2022-04-14 14:20:50 +0300

    добавил webpack

--- src/components/FormFunc/Author.jsx
+++ src/components/FormFunc/Author.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const Author = (props) => {
   return <input className='author' type='text' value={props.author} onChange={props.change} placeholder='Who are you?' />
 }
--- src/components/FormFunc/Button.jsx
+++ src/components/FormFunc/Button.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const Button = (props) => {
   return <button className='button' onClick={props.click}>{props.name}</button>
 }
