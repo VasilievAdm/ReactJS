46eeaaeba0248c3facd25f45846c1a5fedd38186
VasilievAdm <vasiliev.adm@gmail.com>
2022-05-23 23:00:00 +0300

    Update selector.ts

--- src/store/chats/selector.ts
+++ src/store/chats/selector.ts
@@ -4,7 +4,7 @@ import { StoreState } from '..';
 export const selectChats = (state: StoreState) => state.chats;
 
 export const selectChatList = (state: StoreState) =>
-  Object.entries(state.chats).map((chat) => ({
+  Object.keys(state.chats).map((chat) => ({
     id: nanoid(),
-    name: chat[0],
+    name: chat,
   }));
