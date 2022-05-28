69fd54b08fe0fdfee4c9d75a6ba922cc13926ab1
VasilievAdm <vasiliev.adm@gmail.com>
2022-05-28 11:24:30 +0300

    firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBrv2kbNDE6XPef_DTeOlVJ6SJ_WzuxYWY',
+  authDomain: 'gb-react-22949.firebaseapp.com',
+  projectId: 'gb-react-22949',
+  storageBucket: 'gb-react-22949.appspot.com',
+  messagingSenderId: '116234565650',
+  appId: '1:116234565650:web:213a2770e810729d6c80cb',
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export const auth = getAuth(firebase);
+
+export const signUp = async (email: string, password: string) =>
+  await createUserWithEmailAndPassword(auth, email, password);
+
+export const logIn = async (email: string, password: string) =>
+  await signInWithEmailAndPassword(auth, email, password);
+
+export const logOut = async () => await signOut(auth);
